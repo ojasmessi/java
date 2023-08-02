1c1a2f242e689fe90cfb03ae1dd65ef634861327
ojasmessi <oja2001pat@gmail.com>
2023-08-02 12:03:06 +0530

    Palindrome Pyramid

--- pattern12.java
+++ pattern12.java
@@ -1,5 +1,26 @@
+/*
+ pattern: 
+     1         n = 5   
+    2 2       space = n - i  (i=1 -> 5 - 1 = 4  -> n-i)
+   3 3 3      
+  4 4 4 4
+ 5 5 5 5 5
+ */
 public class pattern12 {
-    public static void main(String args[]){
-    
+    public static void main(String args[]) {
+
+        int n = 5;
+        for (int i = 1; i <= n; i++) {
+
+            // spaces
+            for (int j = 1; j <=n - i; j++) {
+                System.out.print(" ");
+            }
+            // numbers -> print row number, row number times
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
     }
 }
