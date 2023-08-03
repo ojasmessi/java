2dc3f172cbd77f6d11ac97705e563dca963ee7bf
ojasmessi <oja2001pat@gmail.com>
2023-08-03 16:47:07 +0530

    .

---
+++ functionSum.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class functionSum {
+
+    public static int CalculateSum(int a, int b){
+        int sum = a + b;
+        return sum;
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int sum = CalculateSum(a, b);
+        System.out.println("Sum of 2 numbers are: " + sum);
+        
+    
+    }
+}
