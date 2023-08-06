b60c2ad561e9588f72ae895c0f5b3a86b40b0a1f
ojasmessi <oja2001pat@gmail.com>
2023-08-06 22:05:38 +0530

    String

---
+++ Lessons/Strings.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Strings {
+    public static void main(String args[]) {
+
+        // concatenation
+        String firstname = "ojas";
+        String lastname = "patil";
+        String fullname = firstname + " " + lastname;
+        System.out.println(fullname.length()); // length -> gives the spaces number
+        // Scanner sc = new Scanner(System.in);
+        // String names = sc.nextLine(); // next -> gives name only
+        // nextLine -> gives full name
+        // System.out.println("The name is :" + names);
+
+        // charAt -> prints every letters
+        for (int i = 0; i < fullname.length(); i++) {
+            System.out.println(fullname.charAt(i));
+        }
+
+    }
+}
