3b429119069209befe21d15954a7aca2df8ac6d8
ojasmessi <oja2001pat@gmail.com>
2023-08-06 22:06:14 +0530

    substring string

---
+++ Lessons/Strings2.java
@@ -0,0 +1,9 @@
+public class Strings2 {
+    public static void main(String args[]) {
+
+        // substring -> (beg index , end index)
+        String sentence = "My name is ojas";
+        String name = sentence.substring(11, sentence.length());
+        System.out.println(name);
+    }
+}
