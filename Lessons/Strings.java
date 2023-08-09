9a7d1abf3a54cabcc1f4343fb672c1a76feac984
ojasmessi <oja2001pat@gmail.com>
2023-08-09 23:23:15 +0530

    String

--- Lessons/Strings.java
+++ Lessons/Strings.java
@@ -1,22 +1,38 @@
-import java.util.*;
+// import java.util.*;
 
+// public class Strings {
+//     public static void main(String args[]) {
+
+//         // concatenation
+//         String firstname = "ojas";
+//         String lastname = "patil";
+//         String fullname = firstname + " " + lastname;
+//         System.out.println(fullname.length()); // length -> gives the spaces number
+//         // Scanner sc = new Scanner(System.in);
+//         // String names = sc.nextLine(); // next -> gives name only
+//         // nextLine -> gives full name
+//         // System.out.println("The name is :" + names);
+
+//         // charAt -> prints every letters
+//         for (int i = 0; i < fullname.length(); i++) {
+//             System.out.println(fullname.charAt(i));
+//         }
+
+//     }
+//To String Method to String class
 public class Strings {
-    public static void main(String args[]) {
-
-        // concatenation
-        String firstname = "ojas";
-        String lastname = "patil";
-        String fullname = firstname + " " + lastname;
-        System.out.println(fullname.length()); // length -> gives the spaces number
-        // Scanner sc = new Scanner(System.in);
-        // String names = sc.nextLine(); // next -> gives name only
-        // nextLine -> gives full name
-        // System.out.println("The name is :" + names);
-
-        // charAt -> prints every letters
-        for (int i = 0; i < fullname.length(); i++) {
-            System.out.println(fullname.charAt(i));
-        }
-
-    }
+   public static void main(String args[]) {
+      int number = 123;
+      String str = Integer.toString(number);
+      System.out.println(str.length());
+      
+      
+   }
 }
+
+           
+           
+        
+     
+     
+// }
